d5a343c5b7ce19c5439efbccdf5b5fdf6e39d18c
mohameddwedar689 <mohameddwedar689@gmail.com>
2023-08-12 16:44:38 +0300

    69. Sqrt(x) [solved]

---
+++ LeetCode Solution/69. Sqrt(x).cpp
@@ -0,0 +1,18 @@
+/*
+    Author: Mohamed Dwedar
+*/
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0 || x == 1)
+            return x;
+        
+        long long i = 1 , res = 1;
+        while(res <= x)
+        {
+            i++;
+            res = i * i;
+        }
+        return i - 1;
+    }
+};
